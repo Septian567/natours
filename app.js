31f34daf086c0823c4760acfb56ada6fc842e47e
Septian Dika Maulana <C2318G2774@bangkit.academy>
2024-04-19 06:47:11 +0700

    .

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+app.options('*', cors());
 
 //  Set security HTTP headers
 app.use(
